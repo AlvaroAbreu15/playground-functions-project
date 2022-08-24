1a30b7e9f91257254ce0ae3b81de3ef900a1bd1a
AlvaroAbreu <alvaroabreu1509@gmail.com>
2022-08-24 17:31:59 -0300

    oitavo requisito terminado

--- src/challenges.js
+++ src/challenges.js
@@ -67,13 +67,30 @@ function catAndMouse(mouse, cat1, cat2) {
     return ('os gatos trombam e o rato foge');
   }
 }
-console.log(catAndMouse(5, 9, 3));
+
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+function fizzBuzz(array) {
 
+  let arrayFinal = [];
+  for (let i = 0; i < array.length ; i += 1){
+        
+    if ( array[i]%3===0 && array[i]%5 !== 0 ) {
+      arrayFinal.push('fizz');
+    }
+     else if ( array[i]%5===0 && array[i]%3 !== 0 ) {
+      arrayFinal.push('buzz');
+     }
+     else if (array[i]%5===0 && array[i]%3 === 0 ){
+      arrayFinal.push('fizzBuzz');
+     }
+     else {
+      arrayFinal.push('bug!');
+     } 
+  }
+  return arrayFinal;
+}
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode() {
   // seu código aqui
