d87b51e5d066de6527005d2dd3affe0ab00ad946
AlvaroAbreu <alvaroabreu1509@gmail.com>
2022-08-25 14:25:45 -0300

    requisito 13 terminado

--- src/challenges2.js
+++ src/challenges2.js
@@ -24,12 +24,27 @@ function triangleCheck(lineA, lineB, lineC) {
     return false;
   }
 }
-console.log(triangleCheck(10, 14, 8));
+
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let reg = /\d+/g;
+ 
+  let result = string.match(reg);
+  let sum = 0;
+  if (result.length > 1) {
+    for (let i = 0; i < result.length ; i += 1){
+      let num = parseInt(result[i]);
+      sum = sum + num;
+    }
+    return (sum + ' copos de água');
+  }
+  else {
+    let num1 = parseInt(result[0]);
+    return (num1 + ' copo de água');
+  }
 }
+console.log(hydrate('4 caipirinhas e 2 cervejas'));
 
 module.exports = {
   generatePhoneNumber,
